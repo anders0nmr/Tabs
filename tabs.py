a117375d4b6bb131493214fe85487e89897360be
mryananderson1 <matthanderson@deloitte.com>
2019-09-23 15:40:35 -0400

    Initial commit

---
+++ tabs.py
@@ -0,0 +1,4 @@
+#python needs at least one tab to know if it is part of previous lines code
+for i in range(1, 12):
+    print('No {} squared is {} and cubed is {:4}'.format(i, i**2, i**4))
+#hitting backspace allows us to create a new block (unit) of code
